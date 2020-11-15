cb476a1c9e36f3db545d1bd071bb234a9ec2e1dd
Kirill Lee <cyril.lee1997@gmail.com>
2020-11-15 16:36:09 +0200

    Updates

--- goit-js-hw-13-image-finder.e31bb0bc.js
+++ goit-js-hw-13-image-finder.e31bb0bc.js
@@ -2633,7 +2633,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59894" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53652" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
